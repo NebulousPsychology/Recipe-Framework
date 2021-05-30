975cab8de7ce6beb1f024e6c223fffc6567a7ed1
NebulousPsychology <cooperativethunder+ghub@outlook.com>
2021-05-30 11:15:14 -0700

    formatting

--- src/RecipeToMarkdown/HumanUnitConverter.cs
+++ src/RecipeToMarkdown/HumanUnitConverter.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 
 namespace RecipeToMarkdown
 {
-    enum ConvertableVolumeUnit {
-        Cup_us,Tbs_us,tsp_us,Tbs_uk,tsp_uk,mL
+    enum ConvertableVolumeUnit
+    {
+        Cup_us, Tbs_us, tsp_us, Tbs_uk, tsp_uk, mL
     }
     /// <summary>
     /// A tool for converting decimal numbers of a given cooking unit into a mixed number of a 'reasonable' unit
@@ -12,8 +13,9 @@ namespace RecipeToMarkdown
     /// <example>
     /// 100 Tbs = 6 1/4 cups
     /// </example>
-    public class HumanUnitConverter{
-           static Dictionary<double, string> FractionLookup = new Dictionary<double, string>(){
+    public class HumanUnitConverter
+    {
+        static Dictionary<double, string> FractionLookup = new Dictionary<double, string>(){
                 {0.25, "¼"},
                 {0.5, "½"},
                 {0.75, "¾"},
@@ -24,16 +26,19 @@ namespace RecipeToMarkdown
                 {3.0/5.0, "⅗"},
                 {4.0/5.0, "⅘"},
             };
-        static string ToMixedNumber(double value){
+
+        static string ToMixedNumber(double value)
+        {
             int whole = (int)Math.Floor(value);
             double decPart = value - whole;
             // fixme: floating point math
             bool mixable = FractionLookup.ContainsKey(decPart);
-            string fraction = mixable? FractionLookup[decPart]:null;
-            return mixable? $"{whole} {fraction}" : value.ToString();
+            string fraction = mixable ? FractionLookup[decPart] : null;
+            return mixable ? $"{whole} {fraction}" : value.ToString();
         }
-        static Tuple<string,string> Convert(double qty, string unit ){
-            return new Tuple<string,string>( ToMixedNumber(qty), unit);
+        static Tuple<string, string> Convert(double qty, string unit)
+        {
+            return new Tuple<string, string>(ToMixedNumber(qty), unit);
         }
     }
 }
